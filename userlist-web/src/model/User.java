9faadcf68ef2b97ecf95d227276d36218f037b1b
alexh <alexh@192.168.1.114>
2018-10-23 20:34:18 -0700

    pom file
    Change-Id: Ib7aa4defedccd1d3512088520ca2586eb1c0fd99

--- userlist-web/src/model/User.java
+++ userlist-web/src/model/User.java
@@ -8,7 +8,7 @@ import javax.persistence.*;
  * 
  */
 @Entity
-@Table(name = "Users")
+@Table(name = "users")
 @NamedQuery(name = "User.findAll", query = "SELECT u FROM User u")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
